b6b2b6b6723264b3c9d4ad784d19c5b7e77eb33b
chenpeng <chenpeng@xiaoniu.com>
2019-01-11 18:09:35 +0800

    Initial commit

---
+++ src/main/java/com/cp/framework/webmvc/servlet/DispatcherServlet.java
@@ -0,0 +1,31 @@
+package com.cp.framework.webmvc.servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Description:
+ *
+ * @author chenpeng
+ * @date 2019/1/11 16:49
+ */
+public class DispatcherServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+}
